5460b1bcf12b0437e5cca7ee84acdb2e3f2736ba
irfanizudin <irfanizzudyn@gmail.com>
2022-10-14 17:30:50 +0700

    Initial Commit

---
+++ instagram-swiftui/instagram_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_swiftuiApp.swift
+//  instagram-swiftui
+//
+//  Created by Irfan Izudin on 14/10/22.
+//
+
+import SwiftUI
+
+@main
+struct instagram_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
